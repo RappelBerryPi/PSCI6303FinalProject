6e197674f7de207a680b996acf7a3d0b3667bfd0
RappelBerryPi <leppanayr@yahoo.com>
2021-11-29 18:39:35 -0600

    Adding in python code to generate fake experiment data

---
+++ generate_fake_data_for_policy_analysis.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import random
+
+voting_preference_major = ["D", "R"]
+voting_preference_minor = ["O", "I"]
+voting_plan_2024_major = ["D", "R"]
+voting_plan_2024_minor = ["O", "I"]
+
+random.seed(1234)
+with open("0-data/experiment_data.csv", "w") as f:
+    f.write("general_voting_preference, number_of_troll_tweets_seen, number_of_troll_tweets_interacted_with, number_of_troll_tweets_reported, 2024_voting_plan\n")
+    for i in range(0, 500):
+
+        # select general_voting_preference
+        general_voting_preference = random.choice(voting_preference_major)
+        if (random.random() < 0.25):
+            general_voting_preference = random.choice(voting_preference_minor)
+
+        # select voting plan for 2024
+        voting_plan_2024 = random.choice(voting_plan_2024_major)
+        if (random.random() < 0.25):
+            voting_plan_2024 = random.choice(voting_plan_2024_minor)
+
+        # randomly generate the effected and non-effected users
+        if (random.random() > 0.6):
+            number_of_troll_tweets_interacted_with = 0
+            number_of_troll_tweets_reported = 0
+            number_of_troll_tweets_seen = 0
+            old_voting_plan = voting_plan_2024
+            voting_plan_2024 = general_voting_preference
+            if (random.random() < 0.1):
+                voting_plan_2024 = old_voting_plan
+        else:
+            number_of_troll_tweets_seen = random.randint(0, 250)
+            number_of_troll_tweets_interacted_with = random.randint(0, number_of_troll_tweets_seen)
+            number_of_troll_tweets_reported = random.randint(0, number_of_troll_tweets_seen)
+        f.write("{},{},{},{},{}\n".format(general_voting_preference, number_of_troll_tweets_seen, number_of_troll_tweets_interacted_with, number_of_troll_tweets_reported, voting_plan_2024))
